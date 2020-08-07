963a2486208725b0ede04d3b648adcdf92a0a249
gongjian <jian.gong@istuary.com>
2020-08-08 00:10:34 +0800

    更新

--- TowerLoadCals.BLL/Electric/ElectricalCalsToolBox3.cs
+++ TowerLoadCals.BLL/Electric/ElectricalCalsToolBox3.cs
@@ -95,108 +95,144 @@ namespace TowerLoadCals.BLL.Electric
                     result = (float)(mianji * elas * Math.Pow(cosB, 5) / 6 / L1 * sum);
                     break;
                 case 3:
-                b[0] = span: b[1] = span - jyzLength1: b[2] = span - yxDist2: b[3] = jyzLength2
-                P[0] = jyzLoad1: P[1] = dxLoad: P[2] = dxLoad: P[3] = jyzLoad2
-                dl[0] = jyzLength1: dl[1] = yxDist2 - jyzLength1: dl[2] = span - yxDist2 - jyzLength2: dl[3] = jyzLength2
-                qq[1] = 0: qq[2] = yxLoad2* P[1] * cosB: qq[3] = 0
-                Q[0] = 1 / b[0] * (dl[0] * P[0] * (b[0] - dl[0] / 2) + dl[1] * P[1] * (b[1] - dl[1] / 2) _
-                        + dl[2] * P[2] * (b[2] - dl[2] / 2) + dl[3] * P[3] * (b[3] - dl[3] / 2) + _
-                        qq[1] * b[1] + qq[2] * b[2] + qq[3] * b[3])
-                Q1[0] = Q[0] - dl[0] * P[0]
-                Q[1] = Q1[0] - qq[1]
-                Q1[1] = Q[1] - dl[1] * P[1]
-                Q[2] = Q1[1] - qq[2]
-                Q1[2] = Q[2] - dl[2] * P[2]
-                Q[3] = Q1[2] - qq[3]
-                Q1[3] = Q[3] - dl[3] * P[3]
-                For i = 0 To 3
-                    sum1 = (Q(i) ^ 3 - Q1(i) ^ 3) / P(i)
-                    sum = sum + sum1
-                Next i
-                kzhi2 = mianji * elas * cosB ^ 5 / 6 / L1 * sum
-            case 4
+                    b[0] = span; b[1] = span - jyzLength1; b[2] = span - yxDist2; b[3] = jyzLength2;
+                    P[0] = jyzLoad1; P[1] = dxLoad; P[2] = dxLoad; P[3] = jyzLoad2;
+                    dl[0] = jyzLength1; dl[1] = yxDist2 - jyzLength1; dl[2] = span - yxDist2 - jyzLength2; dl[3] = jyzLength2;
+                    qq[1] = 0; qq[2] = yxLoad2 * P[1] * cosB; qq[3] = 0;
+                    Q[0] = 1 / b[0] * (dl[0] * P[0] * (b[0] - dl[0] / 2) + dl[1] * P[1] * (b[1] - dl[1] / 2) + dl[2] * P[2] * (b[2] - dl[2] / 2) + dl[3] * P[3] * (b[3] - dl[3] / 2) + qq[1] * b[1] + qq[2] * b[2] + qq[3] * b[3]);
+                    Q1[0] = Q[0] - dl[0] * P[0];
+                    Q[1] = Q1[0] - qq[1];
+                    Q1[1] = Q[1] - dl[1] * P[1];
+                    Q[2] = Q1[1] - qq[2];
+                    Q1[2] = Q[2] - dl[2] * P[2];
+                    Q[3] = Q1[2] - qq[3];
+                    Q1[3] = Q[3] - dl[3] * P[3];
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        sum1 = (float)((Math.Pow(Q[i], 3) - Math.Pow(Q1[i], 3)) / P[i]);
+                        sum = sum + sum1;
+                    }
+                    result = (float)(mianji * elas * Math.Pow(cosB, 5) / 6 / L1 * sum);
+                    break;
+                case 4:
+                    b[0] = span; b[1] = span - jyzLength1; b[2] = span - yxDist1; b[3] = span - yxDist2; b[4] = jyzLength2;
+                    P[0] = jyzLoad1; P[1] = dxLoad; P[2] = dxLoad; P[3] = dxLoad; P[4] = jyzLoad2;
+                    dl[0] = jyzLength1; dl[1] = yxDist1 - jyzLength1; dl[2] = yxDist2 - yxDist1;
+                    dl[3] = span - yxDist2 - jyzLength2; dl[4] = jyzLength2;
+                    qq[1] = 0; qq[2] = yxLoad1 * P[1] * cosB; qq[3] = yxLoad2 * P[2] * cosB; qq[4] = 0;
+                    Q[0] = 1 / b[0] * (dl[0] * P[0] * (b[0] - dl[0] / 2) + dl[1] * P[1] * (b[1] - dl[1] / 2) + dl[2] * P[2] * (b[2] - dl[2] / 2)
+                        + dl[3] * P[3] * (b[3] - dl[3] / 2) + dl[4] * P[4] * (b[4] - dl[4] / 2) + qq[1] * b[1] + qq[2] * b[2] + qq[3] * b[3] + qq[4] * b[4]);
+                    Q1[0] = Q[0] - dl[0] * P[0];
+                    Q[1] = Q1[0] - qq[1];
+                    Q1[1] = Q[1] - dl[1] * P[1];
+                    Q[2] = Q1[1] - qq[2];
+                    Q1[2] = Q[2] - dl[2] * P[2];
+                    Q[3] = Q1[2] - qq[3];
+                    Q1[3] = Q[3] - dl[3] * P[3];
+                    Q[4] = Q1[3] - qq[4];
+                    Q1[4] = Q[4] - dl[4] * P[4];
+                    for (int i = 0; i <= 4; i++)
+                    {
+                        sum1 = (float)((Math.Pow(Q[i], 3) - Math.Pow(Q1[i], 3)) / P[i]);
+                        sum = sum + sum1;
+                    }
 
-                b[0] = span: b[1] = span - jyzLength1: b[2] = span - yxDist1: b[3] = span - yxDist2: b(4) = jyzLength2
-                P[0] = jyzLoad1: P[1] = dxLoad: P[2] = dxLoad: P[3] = dxLoad: P(4) = jyzLoad2
-                dl[0] = jyzLength1: dl[1] = yxDist1 - jyzLength1: dl[2] = yxDist2 - yxDist1
-                    dl[3] = span - yxDist2 - jyzLength2: dl(4) = jyzLength2
-                qq[1] = 0: qq[2] = yxLoad1* P[1] * cosB: qq[3] = yxLoad2* P[2] * cosB: qq(4) = 0
-                Q[0] = 1 / b[0] * (dl[0] * P[0] * (b[0] - dl[0] / 2) + dl[1] * P[1] * (b[1] - dl[1] / 2) _
-                        + dl[2] * P[2] * (b[2] - dl[2] / 2) + dl[3] * P[3] * (b[3] - dl[3] / 2) + _
-                        dl(4) * P(4) * (b(4) - dl(4) / 2) + qq[1] * b[1] + qq[2] * b[2] + qq[3] * b[3] + qq(4) * b(4))
-                Q1[0] = Q[0] - dl[0] * P[0]
-                Q[1] = Q1[0] - qq[1]
-                Q1[1] = Q[1] - dl[1] * P[1]
-                Q[2] = Q1[1] - qq[2]
-                Q1[2] = Q[2] - dl[2] * P[2]
-                Q[3] = Q1[2] - qq[3]
-                Q1[3] = Q[3] - dl[3] * P[3]
-                Q(4) = Q1[3] - qq(4)
-                Q1(4) = Q(4) - dl(4) * P(4)
-                For i = 0 To 4
-                    sum1 = (Q(i) ^ 3 - Q1(i) ^ 3) / P(i)
-                    sum = sum + sum1
-                Next i
-                kzhi2 = mianji * elas * cosB ^ 5 / 6 / L1 * sum
-        End Select
+                    result = (float)(mianji * elas * Math.Pow(cosB, 5) / 6 / L1 * sum);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            return result;
+        }
 
-}
+        //以下为求各工况孤立档的张力函数
+        public static float zhangli(float elas, float expa, float cosB, float mianji, float span, float dl, float tenYZ, float KzhiYZ, float tempYZ, float KzhiJG,
+             float tempJG, string gongkuang, float jyzLength1, float jyzLength2)
+        {
+            //依次输入电线弹性模量、线膨胀系数、余弦值、截面积、档距、过牵引长度、控制工况张力、控制工况K值、控制工况温度、
+            //待求工况K值、待求工况温度、控制工况、绝缘子串1长度、绝缘子串2长度
+            float a, b, c, d, e;
+            float zhangli;
 
-'以下为求各工况孤立档的张力函数
-public static float zhangli(elas float, expa float, cosB float, mianji float, span float, dl float, _
-                tenYZ float, KzhiYZ float, tempYZ float, KzhiJG float, tempJG float, _
-                gongkuang As String, jyzLength1 float, jyzLength2 float) float
-                
-    '依次输入电线弹性模量、线膨胀系数、余弦值、截面积、档距、过牵引长度、控制工况张力、控制工况K值、控制工况温度、
-            '待求工况K值、待求工况温度、控制工况、绝缘子串1长度、绝缘子串2长度
+            if (gongkuang != "过牵引") {
+                a = KzhiYZ / (tenYZ * tenYZ) - tenYZ + expa * elas * mianji * cosB * (tempJG - tempYZ);
+                b = KzhiJG;
+            }
+            else{
+                a = KzhiYZ / (tenYZ * tenYZ) - tenYZ + expa * elas * mianji * cosB * (tempJG - tempYZ) + elas * mianji * dl * cosB / (span - jyzLength1 - jyzLength2);
+                b = KzhiJG;
+            }
 
+            if (a > 0) {
+                c = 1;
+            }
+            else if (a == 0){
+                c = 0;
+            }
+            else{
+                c = -1;
+            }
+            d = (float)(13.5 * b / Math.Pow(Math.Abs(a), 3) - c);
 
-    Dim a float: Dim b float: Dim c float: Dim d float: Dim e float
-    if( gongkuang<> "过牵引" ){
-        a = KzhiYZ / (tenYZ * tenYZ) - tenYZ + expa * elas * mianji * cosB * (tempJG - tempYZ)
-        b = KzhiJG
-    Else
-        a = KzhiYZ / (tenYZ * tenYZ) - tenYZ + expa * elas * mianji * cosB * (tempJG - tempYZ) _
-            + elas* mianji * dl* cosB / (span - jyzLength1 - jyzLength2)
-        b = KzhiJG
-    }
+            //switch(d)
+            //    case Is >= 1
+            //        e = Log(d + Sqr(d* d - 1))
+            //        zhangli = Abs(a) * (Exp(e / 3) + Exp(-e / 3) - c) / 3
+            //    case Is <= 1
+            //        e = Atn(-d / Sqr(1 - d* d)) + 2 * Atn[1]
+            //        zhangli = Abs(a) * (2 * Cos(e / 3) - c) / 3
+            //    case 0
+            //        zhangli = b ^ (1 / 3)
 
-        c = Sgn(a)
-        d = 13.5 * b / Abs(a) ^ 3 - c
+            if(d >= 1){
+                e = (float)Math.Log(d + Math.Sqrt(d * d - 1));
+                zhangli = (float)(Math.Abs(a) * (Math.Exp(e / 3) + Math.Exp(-e / 3) - c) / 3);
+            }
+            else if(d <=1 ){
+                e = (float)(Math.Atan(-d / Math.Sqrt(1 - d * d)) + 2 * Math.Atan(1));
+                zhangli = (float)(Math.Abs(a) * (2 * Math.Cos(e / 3) - c) / 3);
+            }
+            else{
+                zhangli = (float)Math.Pow(b, (1 / 3));
+            }
+            return zhangli;
+        }
+        
+        public static float zhangli1(float a, float b)
+        {
+            float c, d, e;
+            float zhangli1;
 
-        switch( d
-            case Is >= 1
-                e = Log(d + Sqr(d* d - 1))
-                zhangli = Abs(a) * (Exp(e / 3) + Exp(-e / 3) - c) / 3
-            case Is <= 1
-                e = Atn(-d / Sqr(1 - d* d)) + 2 * Atn[1]
-                zhangli = Abs(a) * (2 * Cos(e / 3) - c) / 3
-            case 0
-                zhangli = b ^ (1 / 3)
-        End Select
+            if (a > 0){
+                c = 1;
+            }
+            else if (a == 0){
+                c = 0;
+            }
+            else{
+                c = -1;
+            }
 
-}
-public static float zhangli1(a float, b float) float
+            d = (float)(13.5 * b / Math.Pow(Math.Abs(a), 3) - c);
 
+            if (d >= 1)
+            {
+                e = (float)Math.Log(d + Math.Sqrt(d * d - 1));
+                zhangli1 = (float)(Math.Abs(a) * (Math.Exp(e / 3) + Math.Exp(-e / 3) - c) / 3);
+            }
+            else if (d <= 1)
+            {
+                e = (float)(Math.Atan(-d / Math.Sqrt(1 - d * d)) + 2 * Math.Atan(1));
+                zhangli1 = (float)(Math.Abs(a) * (2 * Math.Cos(e / 3) - c) / 3);
+            }
+            else
+            {
+                zhangli1 = (float)Math.Pow(b, (1 / 3));
+            }
+            return zhangli1;
 
-    Dim c float: Dim d float: Dim e float
-
-
-        c = Sgn(a)
-        d = 13.5 * b / Abs(a) ^ 3 - c
-
-        switch( d
-            case Is >= 1
-                e = Log(d + Sqr(d* d - 1))
-                zhangli1 = Abs(a) * (Exp(e / 3) + Exp(-e / 3) - c) / 3
-            case Is <= 1
-                e = Atn(-d / Sqr(1 - d* d)) + 2 * Atn[1]
-                zhangli1 = Abs(a) * (2 * Cos(e / 3) - c) / 3
-            case 0
-                zhangli1 = b ^ (1 / 3)
-        End Select
-
-}
+        }
 
     }
 
